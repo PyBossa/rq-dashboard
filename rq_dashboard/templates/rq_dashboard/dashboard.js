4e06e81340c3c273c014ced2dc6a47c88e8eb8f3
alejandrodob <alejandrodob@gmail.com>
2014-11-05 11:07:23 +0100

    Add csrf protection for ajax requests

--- rq_dashboard/templates/rq_dashboard/dashboard.js
+++ rq_dashboard/templates/rq_dashboard/dashboard.js
@@ -43,6 +43,19 @@ var api = {
 };
 
 
+(function($) {
+var csrftoken = $('meta[name=csrf-token]').attr('content');
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+})($);
+
+
 //
 // QUEUES
 //
